fc46ad61deed149cc4be7016aea871c4fd15c2c1
Rimlin <Rimlin@gmail.com>
2018-12-25 13:10:07 +0200

    mirva

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,9 @@ public class Main {
         int p = 0;
         if (sc.hasNextInt())
             m = sc.nextInt();
-
         n = sc.nextInt();
-
         l = sc.nextInt();
-
         p = sc.nextInt();
-
         if (m == p || n == l && m == n || n == l || l == p || p == m || m == l || n == p)
             System.out.println(true);
         else System.out.println(false);
