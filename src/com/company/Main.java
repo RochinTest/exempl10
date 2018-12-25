16ab0ad5b465a7a6d2e62a2fd87e5a598bb9f73f
Rimlin <Rimlin@gmail.com>
2018-12-25 13:08:32 +0200

    mirva

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int m = 0, n, l;
+        int p = 0;
+        if (sc.hasNextInt())
+            m = sc.nextInt();
+
+        n = sc.nextInt();
+
+        l = sc.nextInt();
+
+        p = sc.nextInt();
+
+        if (m == p || n == l && m == n || n == l || l == p || p == m || m == l || n == p)
+            System.out.println(true);
+        else System.out.println(false);
+
+    }
+}
